0bca4fdd98ad5f1ea4c831a4f97ddea9e4522c56
Isabel TS <izzytons@gmail.com>
2021-05-19 13:03:55 -0400

    edit module.exports

--- routes/products.js
+++ routes/products.js
@@ -2,5 +2,8 @@ var express = require("express");
 var router = express.Router();
 
 module.exports = function (db) {
+  router.get("/products", () => {
+    return db.get("products").value();
+  });
   return router;
 };
