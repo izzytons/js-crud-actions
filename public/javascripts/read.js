c295fb5e0ba43afb0429ba984b2e985c4e0959c4
Isabel TS <izzytons@gmail.com>
2021-05-19 13:42:29 -0400

    complete load onclick event

--- public/javascripts/read.js
+++ public/javascripts/read.js
@@ -1,3 +1,12 @@
+document.getElementById("load").onclick = function () {
+  const req = new XMLHttpRequest();
+  req.open('GET', '/api/products');
+  req.onload = function () {
+    const data = JSON.parse(req.response);
+    addList({ data });
+  }
+  req.send();
+}
 
 function addList({ data }) {
 
