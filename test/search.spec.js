cf2c6f4fdff4b9aada117aca8d608c9359052ab1
Isabel Tonelli-Sippel <isa03768@tjx.com>
2021-05-27 11:21:21 -0400

    implement first test - fails

--- test/search.spec.js
+++ test/search.spec.js
@@ -1,14 +1,30 @@
-var assert = require('assert');
+const assert = require('assert');
+const httpMocks = require("node-mocks-http");
+const products_router = require("../routes/products");
 
-describe('Basic mocha test', function () {
-    it('should not throw errors', function () {
-        assert.equal(3, 3);
-    });
-});
 
-describe('OnClick Query Synthesis'), function () {
-    it('should create the expected query', function () {
+describe('Test1', function () {
+    /*it('should create the expected query', function () {
         document.getElementById('search-terms').value = 'test';
         document.getElementById("search").click();
+    });*/
+
+    it("should return test product info", function () {
+        const mockRequest = httpMocks.createRequest({
+            method: "POST",
+            url: "/search",
+            keyword: "test"
+        });
+        const mockResponse = httpMocks.createResponse();
+
+        products_router(mockRequest, mockResponse);
+        const actualResponse = mockResponse._getData();
+        const expectedResponse = {
+            id: '0fc062ad-6f8a-41d6-9af6-4d8cc8994543',
+            name: 'test',
+            price: '20.99'
+        };
+
+        assert(actualResponse, expectedResponse);
     });
-};
+});
